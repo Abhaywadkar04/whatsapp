75b4bf0056ff4b8e1171567d2c4c4c1ff73c5f22
Abhaywadkar04 <abhaywadkar2004@gmail.com>
2025-01-19 15:21:34 +0530

    working on profile ui

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,13 +1,113 @@
-import React from 'react'
-import { useAppStore } from '@/store';
+import React from "react";
+import { useAppStore } from "@/store";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { IoArrowBack } from "react-icons/io5";
+import { Avatar } from "@radix-ui/react-avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
+import { getColor } from "@/lib/utils";
+import { FaTrash, FaPlus } from "react-icons/fa";
+import { colors } from "@/lib/utils";
 
-function profile() {
-  const {userInfo}=useAppStore()
+function Profile() {
+  const { userInfo, setUserInfo } = useAppStore();
+  const navigate = useNavigate();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [image, setImage] = useState(null);
+  const [selectedColor, setSelectedColor] = useState(0);
+  const [hovered, setHovered] = useState(false);
+
+  const saveChanges = async () => {};
   return (
-    <div>profile
-      <div>Email:{userInfo.email}</div>
+    <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10">
+      <div className="flex flex-col gap-10 w-[80vw] md:w-max">
+        <div>
+          <IoArrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer" />
+        </div>
+        <div className="grid grid-cols-2">
+          <div
+            className="h-full w-32  md:w-48 md:h-48 relative flex items-center justify-center"
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+          >
+            <Avatar className="h-32 w-32 md:w-48 md:h-48 relative flex items-center justify-center overflow-hidden rounded-full">
+              {image ? (
+                <AvatarImage
+                  src={image}
+                  alt="Profile"
+                  className="object-cover w-full h-full bg-black"
+                />
+              ) : (
+                <div
+                  className={`uppercase h-32 w-32 md:w-48 md:h-48 text-5xl border-[1px] flex items-center justify-center rounded-full ${getColor(
+                    selectedColor
+                  )}`}
+                >
+                  {firstName
+                    ? firstName.split("").shift()
+                    : userInfo.email.split("").shift() || ""}
+                </div>
+              )}
+            </Avatar>
+            {hovered && (
+              <div
+                className="absolute inset-0 ring-  bg-black/50 flex items-center justify-center ring-fuchsia-50 rounded-full
+              "
+              >
+                {image ? (
+                  <FaTrash className="text-white text-3xl cursor-pointer" />
+                ) : (
+                  <FaPlus className="text-white text-3xl cursor-pointer" />
+                )}
+              </div>
+            )}
+            {/* <input type="text" /> */}
+          </div>
+          <div className="flex min-w-32 md:min-w-64 flex-col gap-5 text-white items-center justify-center">
+            <div className="w-full">
+              <input
+                placeholder="Email"
+                type="email"
+                disabled
+                value={userInfo.email || ""}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className="w-full">
+              <input
+                placeholder="First Name"
+                type="text"
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className="w-full">
+              <input
+                placeholder="Second Name"
+                type="text"
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className="w-full flex gap-5">
+              {colors.map((color, index) => (
+                <div
+                  className={`${color} h-8 w-8 rounded-full cursor-pointer transition-all duration-300
+                  ${selectedColor === index ? "outline outline-white/50" : ""}}
+                  `}
+                  key={index}
+                  onClick={() => setSelectedColor(index)}
+                ></div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default profile
+export default Profile;
--- client/src/store/slices/auth-slice.js
+++ client/src/store/slices/auth-slice.js
@@ -3,6 +3,7 @@ import profile from "@/pages/profile";
 export const createAuthSlice = (set) => ({
     userInfo: undefined,
     setUserInfo: (userInfo) => set({ userInfo }),
-    // setProfileSetup: (ProfileSetup) => set({ ProfileSetup }),
+    setProfileSetup: (ProfileSetup) => set({ ProfileSetup }),
+    setEmail: (Email) => set({ Email }),
 });
 
